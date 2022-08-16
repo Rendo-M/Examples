77b2b7b354ca903891d25910489947c2fbbe01d9
Rendo-M <srvr@list.ru>
2022-08-16 19:46:28 +0300

    first 3 examples

---
+++ ex1_hw/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World1");
+        Console.WriteLine("Hello, World2");
+        Console.WriteLine("Hello, World3");        
+    }
+}
---
+++ ex3_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number one ");
+Int32 numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number two ");
+Int32 numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numA + numB);
