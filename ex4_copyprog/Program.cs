33d0ddc0c780c33f9eeb2be46d4aa28384dcf822
Rendo-M <srvr@list.ru>
2022-08-16 22:16:42 +0300

    3 commit

---
+++ ex4_copyprog/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int xa = 1;
+int xb = 35;
+int xc = 35;
+int ya = 1;
+int yb = 1;
+int yc = 7;
+Console.Clear();
+Console.SetCursorPosition(xa, ya);
+Console.Write('+');
+Console.SetCursorPosition(xb,yb);
+Console.Write('+');
+Console.SetCursorPosition(xc,yc);
+Console.Write('+');
+{
+
+}
