8a6d687e2b013bc7f56f27d94943c94bc31350cf
Rendo-M <srvr@list.ru>
2022-08-16 22:15:04 +0300

    2 commit

--- ex3_sum/Program.cs
+++ ex3_sum/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter number one ");
+Console.Write("Enter number one");
 Int32 numA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter number two ");
 Int32 numB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(numA + numB);
+Int32 a = new Random().Next(numA, numB);
+Int32 b = new Random().Next(numA, numB);
+Console.Write(a);
+Console.Write('+');
+Console.Write(b);
+Console.Write('=');
+Console.Write(a+b);
