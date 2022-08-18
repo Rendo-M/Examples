9f5f186f3dbb67db66cd2698315998e0ff8e3304
Rendo-M <srvr@list.ru>
2022-08-18 23:24:14 +0300

    seminar1 task 4

---
+++ s1_task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Input positive int number: ");
+Int32 num = Convert.ToInt32(Console.ReadLine());
+Int32 counter = 2;
+Console.WriteLine($"Even numbers from 1 to {num}:");
+while (counter <= num)
+{
+Console.Write(counter+" ");
+counter += 2;
+}
