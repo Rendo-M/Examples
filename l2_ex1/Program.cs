788f05e052840593943205596f3ef4bc5ba9a511
Rendo-M <srvr@list.ru>
2022-08-20 12:54:15 +0300

    funcs and arrays example lect2

---
+++ l2_ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] array = new int[10];
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int index = collection.Length;
+    while (index > 0)
+    {
+        index--;
+        Console.Write($"({index}, {collection[index]}) ");
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{   int index = 0;
+    int count = collection.Length;
+    while (index<count)
+    {
+        if (collection[index] == find)
+        {
+            return index;
+            break;
+        }
+        index++;    
+    }
+    return -1;
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int lf = new Random().Next(1,10);
+Console.WriteLine($"Looking for position of {lf}");
+Console.WriteLine(IndexOf(array,lf));
