9c87a53f47f1b21d0517a7d6f46c90797cbd4e35
Rendo-M <srvr@list.ru>
2022-08-22 21:25:05 +0300

    seminar2 examples

---
+++ s2_class/Program.cs
@@ -0,0 +1,10 @@
+﻿int GetDigitByNumber(int number, int position)
+{   int divider = Convert.ToInt32(Math.Pow(10,position));
+    int digit = number % divider*10 / divider;
+    return digit;
+}
+Console.Write("Enter a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a position of desired digit: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} digit of number {x} is {GetDigitByNumber(x,n)}");
---
+++ s2_class2/Program.cs
@@ -0,0 +1,36 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли 
+// оно одновременно 7 и 23.
+
+bool IsMultiple(int number, int divider1, int divider2)
+{   
+    return (number % divider1 == 0)&&(number % divider2 == 0);
+}
+
+Console.Write("Enter a number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (IsMultiple(x, 7, 23))
+{
+    Console.WriteLine("ОНО ДЕЛИТСЯ!");
+}
+else
+{
+    Console.WriteLine("Не делится ):");
+} 
+
+// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+ bool IsSquare(int num1, int num2)
+ {
+    return (num1 * num1 == num2)||(num2 * num2 == num1);
+ }
+
+ int first = new Random().Next(1,50);
+ int second = new Random().Next(1,50);
+ if (IsSquare(first, second)){
+ Console.WriteLine($"one of nubmers {first} and {second} is square of anoter"); 
+ }
+ else
+ {
+Console.WriteLine($"no one of nubmers {first} and {second} is square of anoter"); 
+ 
+ }
